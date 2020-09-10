ff32fa53cffccce7dc6a0a6f87ccc7f326efd799
anikurhade <anikurhade2002@gamil.com>
2020-09-10 23:14:30 +0530

    Adding Main Class Of Application

---
+++ src/Linkcode/Main/Main.java
@@ -0,0 +1,17 @@
+package Linkcode.Main;
+
+import Linkcode.Gui.Dashboard;
+
+
+import Linkcode.Gui.TempDashboard;
+import Linkcode.Gui.Welcome;
+
+
+public class Main {
+
+	public static void main(String args[]) {
+		//System.out.println("Welcome");
+		Welcome w1=new Welcome();
+		//Dashboard d=new Dashboard();
+	}
+}
