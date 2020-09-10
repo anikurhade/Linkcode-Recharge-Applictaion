6ae784c1130018bcc7bd3e867b4ae5017903fb59
anikurhade <anikurhade2002@gamil.com>
2020-09-10 23:17:38 +0530

    Adding Interface Folder Of Application

---
+++ src/Linkcode/Interface/mouseAdapt.java
@@ -0,0 +1,51 @@
+package Linkcode.Interface;
+
+import java.awt.event.*;
+
+public interface mouseAdapt extends MouseListener,MouseMotionListener {
+
+	@Override
+	default void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	default void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	default void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	default void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	default void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	default void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	default void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
